61174d8f84fd263664fa35531371138285f752d1
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-04-29 16:12:31 +0900

    Chrome

--- clock.js
+++ clock.js
@@ -1,19 +1,28 @@
-const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+const clockContainer=document.querySelector(".js-clock"),
+time=document.querySelector("h1")
 
-function getTime() {
-  const date = new Date();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+function getTime(){
+    const date=new Date();
+    const hour=date.getHours();
+    const minutes=date.getMinutes();
+    const second=date.getSeconds();
+
+time.innerText=`${hour < 10 ? `0${hour}` : hour} : ${minutes < 10 ? `0${minutes}` :minutes} : ${second < 10 ? `0${second}` : second}`
+
 }
+    
 
-function init() {
-  getTime();
-  setInterval(getTime, 1000);
+
+function init(){
+    getTime();
+setInterval(getTime,1000)
+
 }
 
 init();
+
+
+
+
+
